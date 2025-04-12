a42cb791b8a20f76572c4309d80cde119fc0a7a7
Nakamize <makisjozef28@gmail.com>
2025-04-12 22:29:00 +0200

    Operation class added

---
+++ src/grammar/tone_operations.py
@@ -0,0 +1,45 @@
+import random
+
+class ToneOperator:
+    @staticmethod
+    def transpose(tone, semitones):
+        """Transpose the tone by a given number of semitones."""
+        pitch_classes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+        pitch, octave = tone.pitch[:-1], int(tone.pitch[-1])
+        index = pitch_classes.index(pitch)
+        new_index = (index + semitones) % 12
+        new_octave = octave + (index + semitones) // 12
+        tone.pitch = pitch_classes[new_index] + str(new_octave)
+        return tone
+    
+    @staticmethod
+    def counterpoint(note):
+        # Basic mapping of note names to semitone values in C major
+        note_to_semitone = {
+            "C": 0, "D": 2, "E": 4, "F": 5,
+            "G": 7, "A": 9, "B": 11
+        }
+        semitone_to_note = {v: k for k, v in note_to_semitone.items()}
+
+        # Allowed consonant intervals (in semitones)
+        consonant_intervals = [3, 4, 7, 8, 9, 12]  # m3, M3, P5, m6, M6, P8
+
+
+        base_semitone = note_to_semitone.get(note.upper())
+        if base_semitone is None:
+            raise ValueError(f"Unknown note: {note}")
+
+        # Try all consonant intervals above the melody note
+        possible_notes = []
+        for interval in consonant_intervals:
+            cp_semitone = (base_semitone + interval) % 12
+            if cp_semitone in semitone_to_note:
+                possible_notes.append(semitone_to_note[cp_semitone])
+
+        # Pick one possible counterpoint note at random
+        if possible_notes:
+            chosen = random.choice(possible_notes)
+        else:
+            chosen = "?"  # fallback
+
+        return chosen
